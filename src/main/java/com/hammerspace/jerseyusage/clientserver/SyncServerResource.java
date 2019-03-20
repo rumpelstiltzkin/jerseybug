001bd652c17a7effe4e07d96e5c37234f91a0487
Anand Ganesh <anand@hammerspace.com>
2019-03-20 19:55:28 +0000

    Added a LICENSE; EPL v2.0

--- src/main/java/com/hammerspace/jerseyusage/clientserver/SyncServerResource.java
+++ src/main/java/com/hammerspace/jerseyusage/clientserver/SyncServerResource.java
@@ -1,3 +1,18 @@
+// Copyright (c) 2019 Hammerspace, Inc.
+// 	  www.hammer.space
+//
+// Licensed under the Eclipse Public License - v 2.0 ("the License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.hammerspace.jerseyusage.clientserver;
 
 import org.springframework.stereotype.Component;
